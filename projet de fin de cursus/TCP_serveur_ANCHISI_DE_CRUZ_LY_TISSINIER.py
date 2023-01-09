e8a5f6ceeed0555c95746f1d913a4926d6e4c381
DE CRUZ Océan <ocean.de-cruz@ipsa.Fr>
2023-01-09 14:20:18 +0100

    +

--- projet de fin de cursus/TCP_serveur_ANCHISI_DE_CRUZ_LY_TISSINIER.py
+++ projet de fin de cursus/TCP_serveur_ANCHISI_DE_CRUZ_LY_TISSINIER.py
@@ -1,50 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  7 07:28:31 2023
-
-@author: lyale
-"""
+# TCP Server
+# Server program for robots to communicate
+# Authors: Ambre ANCHISI, Ocean DE CRUZ, Alexandre LY, Axel TISSINIER
+# IPSA SAA AU516 Final project
+# 5 Jan 2023
 
 import socket
 import threading
 
-# Création de la socket serveur
+# Creation of socket server
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Liaison de la socket à une adresse et un port
-server_socket.bind(('192.168.18.10', 5000))
+# Link of socket to an adress and a port
+server_socket.bind(('192.168.1.159', 5000))
 
-# Ecoute de la socket en attente de connexions
+# Listening of socket waiting for a connections
 server_socket.listen()
 
-# Dictionnaire pour stocker les adresses IP des clients
+# dictionary to store client IP addresses
 clients = {}
 
 def handle_client(client_socket, client_address):
-    # Envoi d'un message de bienvenue au client
+    # Sending a welcome message to the customer
     message = "Connection accepted"
     client_socket.send(bytes(message, 'utf-8'))
     print(f"Server send 'Connection accepted' to {client_address}")
 
     while True:
-        # Réception d'un message du client
+        # Receiving a message from the customer
         data = client_socket.recv(1024)
         if not data:
             break
         print(f"Received message from client {client_address}: {data.decode()}")
         
-        # Traitement du message reçu et envoi d'une réponse au client
+        # Processing the message received and sending a response to the customer
         if data.decode() == "Lost ball":
             message = "Search"
-            # Envoi du message "Search" aux autres clients
+            # Send "Search" message to other clientsSend "Search" message to other clients
             for addr, sock in clients.items():
                 if addr != client_address:
                     sock.send(bytes("Search", 'utf-8'))
+                    print(f"Server send 'Search' to {addr}")
         elif data.decode() == "Search" and len(clients) > 1:
             message = "Search"
         elif data.decode() == "Found ball" and len(clients) > 1:
             message = "Leader"
-            # Envoi du message "Follower" aux autres clients
+            # Sending the "Follower" message to other customers
             for addr, sock in clients.items():
                 if addr != client_address:
                     sock.send(bytes("Follower", 'utf-8'))
@@ -54,21 +54,21 @@ def handle_client(client_socket, client_address):
         client_socket.send(bytes(message, 'utf-8'))
         print(f"Server send '{message}' to {client_address}")
         
-    # Fermeture de la socket client
+    # Closing the server socket
     client_socket.close()
 
 while len(clients) < 2:
-    # Acceptation de la connexion d'un client
+    # Accepting a client's connection
     client_socket, client_address = server_socket.accept()
     print(f"Request connection from {client_address}")
     print(f"Connection accepted for {client_address}")
-    # Enregistrement de l'adresse IP du client
+    # Recording of the client's IP address
     clients[client_address] = client_socket
-    # Création d'un thread pour gérer les communications avec le client
+    # Creation of a thread to manage communications with the client
     t = threading.Thread(target=handle_client, args=(client_socket, client_address))
     t.start()
     
-# Si il y a au moins deux clients, envoi du message "Search" à tous les clients
+# If there are at least two clients, the "Search" message is sent to all clients
 for addr, sock in clients.items():
     sock.send(bytes("Search", 'utf-8'))
     print(f"Server send 'Search' to {addr}")
@@ -78,8 +78,7 @@ while x!='Shutdown':
     x = input()
     b = bytes(x, 'utf-8')
     for addr, sock in clients.items():
-        if addr != client_address and x!='stop':
-            sock.send(b)
-            print(f"Server send {x} to {client_address}")
-# Fermeture de la socket serveur
+        sock.send(b)
+        print(f"Server send {x} to {addr}")
+# Closing the server socket
 server_socket.close()
